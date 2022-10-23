89114050caa900e5e25a1fcd0178c99ead9bf817
teach310 <okms310@yahoo.co.jp>
2022-10-23 16:58:57 +0900

    feat MergeBundles
    特筆することはない。

---
+++ Runtime/Tasks/MergeBundles.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace AssetBundleHub.Tasks
+{
+    /// <summary>
+    /// TempからDestにAssetBundleを移動させる
+    /// </summary>
+    public class MergeBundles
+    {
+        public UniTask Run(IBundlePullContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            foreach (var assetBundleName in context.GetTempAssetBundles())
+            {
+                string srcPath = context.GetTempSavePath(assetBundleName);
+                string destPath = context.GetDestPath(assetBundleName);
+                string destDir = Path.GetDirectoryName(destPath);
+                if (!Directory.Exists(destDir))
+                {
+                    Directory.CreateDirectory(destDir);
+                }
+                File.Delete(destPath);
+                File.Move(srcPath, destPath);
+            }
+            return UniTask.CompletedTask;
+        }
+    }
+}
